4c603356ebf0140c4557a162b90d0bfdbf45eae4
Mark Warneke <mawarnek@microsoft.com>
2021-10-12 14:13:04 +0100

    add basic structure

---
+++ src/motor-api/src/config.py
@@ -0,0 +1,19 @@
+import os
+import logging
+
+
+class DefaultConfig:
+
+    # General Stuff
+    RELEASE = os.environ.get("RELEASE", "1.0")
+    API_KEY = os.environ.get("API_KEY", "test")
+    LOCAL_DEV = os.environ.get("LOCAL_DEV", False)
+
+    # API Description
+    API_TASK = os.environ.get("API_TASK", "Motor API")
+    API_TITLE = "Recup Retun Box - {} API".format(API_TASK)
+    API_DESCRIPTION = ""
+    
+    ROOT_PATH = os.environ.get("ROOT_PATH", "/")
+
+    logging.basicConfig(level=os.environ.get("LOGLEVEL", "WARNING"))
---
+++ src/motor-api/src/router/health_router.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter
+
+from controller.health_controller import HealthController
+from model.health_model import HealthResult
+
+
+router = APIRouter()
+
+
+@router.get("", response_model=HealthResult, tags=["Health"])
+async def get_health():
+    """Returns the health status of the API
+    A status `true` is good.
+    ```json
+    {
+        "Status": true
+    }
+    ```
+    """
+    controller = HealthController()
+    status = controller.check()
+    return HealthResult(status=status.status)
