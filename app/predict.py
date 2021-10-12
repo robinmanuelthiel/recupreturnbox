4b955760846f1e10f659686d5ae72ceaa9a511f2
jhchein <hehein@microsoft.com>
2021-10-12 18:40:23 +0200

    sped up ml inference 5-fold

--- app/predict.py
+++ app/predict.py
@@ -5,12 +5,11 @@ import tensorflow as tf
 
 from PIL import Image
 import numpy as np
-import sys
 
 filename = "model.pb"
 labels_filename = "labels.txt"
 
-network_input_size = 0
+# network_input_size = 0
 
 output_layer = "loss:0"
 input_node = "Placeholder:0"
@@ -40,95 +39,16 @@ def initialize():
     print("Loading labels...", end="")
     with open(labels_filename, "rt") as lf:
         global labels
-        labels = [l.strip() for l in lf.readlines()]
+        labels = [label.strip() for label in lf.readlines()]
+
+    tf.compat.v1.reset_default_graph()
+    tf.import_graph_def(graph_def, name="")
+
     print(len(labels), "found. Success!")
 
 
 def log_msg(msg):
     print("{}: {}".format(datetime.now(), msg))
-
-
-def extract_bilinear_pixel(img, x, y, ratio, xOrigin, yOrigin):
-    xDelta = (x + 0.5) * ratio - 0.5
-    x0 = int(xDelta)
-    xDelta -= x0
-    x0 += xOrigin
-    if x0 < 0:
-        x0 = 0
-        x1 = 0
-        xDelta = 0.0
-    elif x0 >= img.shape[1] - 1:
-        x0 = img.shape[1] - 1
-        x1 = img.shape[1] - 1
-        xDelta = 0.0
-    else:
-        x1 = x0 + 1
-
-    yDelta = (y + 0.5) * ratio - 0.5
-    y0 = int(yDelta)
-    yDelta -= y0
-    y0 += yOrigin
-    if y0 < 0:
-        y0 = 0
-        y1 = 0
-        yDelta = 0.0
-    elif y0 >= img.shape[0] - 1:
-        y0 = img.shape[0] - 1
-        y1 = img.shape[0] - 1
-        yDelta = 0.0
-    else:
-        y1 = y0 + 1
-
-    # Get pixels in four corners
-    bl = img[y0, x0]
-    br = img[y0, x1]
-    tl = img[y1, x0]
-    tr = img[y1, x1]
-    # Calculate interpolation
-    b = xDelta * br + (1.0 - xDelta) * bl
-    t = xDelta * tr + (1.0 - xDelta) * tl
-    pixel = yDelta * t + (1.0 - yDelta) * b
-    return pixel
-
-
-def extract_and_resize(img, targetSize):
-    determinant = img.shape[1] * targetSize[0] - img.shape[0] * targetSize[1]
-    if determinant < 0:
-        ratio = float(img.shape[1]) / float(targetSize[1])
-        xOrigin = 0
-        yOrigin = int(0.5 * (img.shape[0] - ratio * targetSize[0]))
-    elif determinant > 0:
-        ratio = float(img.shape[0]) / float(targetSize[0])
-        xOrigin = int(0.5 * (img.shape[1] - ratio * targetSize[1]))
-        yOrigin = 0
-    else:
-        ratio = float(img.shape[0]) / float(targetSize[0])
-        xOrigin = 0
-        yOrigin = 0
-    resize_image = np.empty(
-        (targetSize[0], targetSize[1], img.shape[2]), dtype=np.float32
-    )
-    for y in range(targetSize[0]):
-        for x in range(targetSize[1]):
-            resize_image[y, x] = extract_bilinear_pixel(
-                img, x, y, ratio, xOrigin, yOrigin
-            )
-    return resize_image
-
-
-def extract_and_resize_to_256_square(image):
-    h, w = image.shape[:2]
-    log_msg(
-        "crop_center: "
-        + str(w)
-        + "x"
-        + str(h)
-        + " and resize to "
-        + str(256)
-        + "x"
-        + str(256)
-    )
-    return extract_and_resize(image, (256, 256))
 
 
 def crop_center(img, cropx, cropy):
@@ -228,15 +148,13 @@ def predict_image(image):
         image = convert_to_nparray(image)
 
         # Crop the center square and resize that square down to 256x256
-        resized_image = extract_and_resize_to_256_square(image)
+        # resized_image = extract_and_resize_to_256_square(image)
+        resized_image = tf.image.resize(image, [256, 256])
 
         # Crop the center for the specified network_input_Size
         cropped_image = crop_center(
             resized_image, network_input_size, network_input_size
         )
-
-        tf.compat.v1.reset_default_graph()
-        tf.import_graph_def(graph_def, name="")
 
         with tf.compat.v1.Session() as sess:
             prob_tensor = sess.graph.get_tensor_by_name(output_layer)
