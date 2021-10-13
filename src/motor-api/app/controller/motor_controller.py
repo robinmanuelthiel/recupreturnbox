ad6052cb4660b47446854d9a4a9685ab1f99dd91
Mark Warneke <mawarnek@microsoft.com>
2021-10-13 10:34:22 +0100

    make fast api running

---
+++ src/motor-api/app/controller/motor_controller.py
@@ -0,0 +1,16 @@
+
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class MotorController:
+
+    def __init__(self):
+        pass
+
+    def open(self):
+        return "open"
+
+    def close(self):
+        return "close"
